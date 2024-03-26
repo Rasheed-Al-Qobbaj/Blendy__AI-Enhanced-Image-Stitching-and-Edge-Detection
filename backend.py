725475fb98ca5aa7699dc38de5b976ba2eeb59cc
Rashid Al-Qobbaj <1202474@student.birzeit.edu>
2024-03-26 14:09:27 +0200

    Cleaned code by implementing functions and add sample images from https://github.com/SSARCandy/panoramas-image-stitching

--- backend.py
+++ backend.py
@@ -1,51 +1,66 @@
 import numpy as np
 import cv2
+import os
 
 # Image Selection and Stitching
-left = cv2.imread("photos/stitch_left.jpg")
-right = cv2.imread("photos/stitch_right.jpg")
+def select_images(Path):
+    image_paths = [Path + '/' + f for f in os.listdir(path=Path)]
+    images = []
+    for path in image_paths:
+        image = cv2.imread(path)
+        images.append(image)
+    return images
 
-stitcher = cv2.Stitcher_create()
 
-status, result = stitcher.stitch([left, right])
-
-if status == cv2.Stitcher_OK:
-    cv2.imwrite("photos/stitch_result.jpg", result)
-    print("Stitching successful!")
-else:
-    print("Stitching failed!")
+# Stitching
+def stitch_images(images):
+    stitcher = cv2.Stitcher_create()
+    status, result = stitcher.stitch(images)
+    if status == cv2.Stitcher_OK:
+        cv2.imwrite("result/stitch_result.jpg", result)
+        print("Stitching successful!")
+        return 1
+    return status
 
 
 # Edge Detection Implementation
 
-stitched = cv2.imread("photos/stitch_result.jpg")
-
 # Apply Canny Edge Detection
-gray = cv2.cvtColor(stitched, cv2.COLOR_BGR2GRAY)
-v = np.median(gray)
-lower = int(0.68 * v)
-upper = int(1.32 * v)
+def canny_edge_detection(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    v = np.median(gray)
+    lower = int(0.68 * v)
+    upper = int(1.32 * v)
+    canny = cv2.Canny(gray, lower, upper)
+    cv2.imwrite("result/canny_edges.jpg", canny)
+    print("Canny Edge detection complete!")
+    return canny
 
-# Apply automatic Canny edge detection using the computed thresholds
-canny = cv2.Canny(gray, lower, upper)
 
-cv2.imwrite("photos/canny_edges.jpg", canny)
-
-print("Canny Edge detection complete!")
 
 # Implement Difference of Gaussians (DoG) edge detection followed by a morphological operation to clean the results
-# Apply GaussianBlur
-blurred = cv2.GaussianBlur(gray, (7, 7), 1)
-
-# Apply Difference of Gaussians
-dog = cv2.subtract(blurred, cv2.GaussianBlur(blurred, (19, 19), 3))
-
-# Apply morphological operation
-kernel = np.array([[0, 1, 0],
+def dog_edge_detection(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (7, 7), 1)
+    dog = cv2.subtract(blurred, cv2.GaussianBlur(blurred, (19, 19), 3))
+    kernel = np.array([[0, 1, 0],
                           [1, 1, 1],
                           [0, 1, 0]], np.uint8)
-dog = cv2.morphologyEx(dog, cv2.MORPH_CLOSE, kernel)
+    dog = cv2.morphologyEx(dog, cv2.MORPH_CLOSE, kernel)
+    cv2.imwrite("result/dog_edges.jpg", dog)
+    print("Difference of Gaussians (DoG) edge detection complete!")
+    return dog
 
-cv2.imwrite("photos/dog_edges.jpg", dog)
+if __name__ == '__main__':
+    images = select_images('input_image/parrington')
+    result = stitch_images(images)
+    if result == 1:
+        stitched = cv2.imread("result/stitch_result.jpg")
+        canny_edge_detection(stitched)
+        dog_edge_detection(stitched)
+    else:
+        print("Stitching failed! Status code: ", result)
 
-print("Difference of Gaussians (DoG) edge detection complete!")
+# Todo: 1- Implement AI human detection using YOLO
+# Todo: 2- Start designing the frontend and backend using fastapi
+# Todo: 3- Report the results
